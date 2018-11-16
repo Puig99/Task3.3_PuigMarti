fa9390c30ac8163dc6d23b082847987f1ef91786
unknown <puigmarti99@gmail.com>
2018-11-16 19:46:46 +0100

    PHP works

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Task3.3_PuigMarti
+
+#### Name: Leap years
+
+#### Author: Marti Puig Albuquerque
+
+#### Subject: IAW
+
+#### School year: 2nd ASIX
+
+---
+## Task 
+
+We were asked to show on the screen the leap years from 1 to 3000 AD. 
+
+The numbers are shown in cards, that were generated with bootstrap.
+
+---
+## Requirements
+
+To see the page in the correct way we will need a web navigator,  and an Apache server since the page is written in PHP.
+
+### PHP 
+![PHPimage](https://avatars0.githubusercontent.com/u/25158?s=280&v=4)
+
+PHP is a popular general-purpose scripting language that is especially suited to web development.
+Fast, flexible and pragmatic, PHP powers everything from your blog to the most popular websites in the world.
+
+It requires an Apacher server in order to be played in a web navigator correctly
+
+
+### Apache server
+
+We will be getting our Apacher server from the application XAMPP, that also includes another servers, so we can continue using it in the future
+
+XAMPP is a free and open-source cross-platform web server solution stack package developed by Apache Friends, consisting mainly of the Apache HTTP Server, MariaDB database, and interpreters for scripts written in the PHP and Perl programming languages. Since most actual web server deployments use the same components as XAMPP, it makes transitioning from a local test server to a live server possible.
+
+![XAMPPimage](https://cdn2.iconfinder.com/data/icons/pack1-baco-flurry-icons-style/512/XAMPP.png)
+
+---
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title> Leap years</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+    <div class="text-center">
+        <h1> Leap Years </h1>
+            <h2 class="text-muted"> From 1 to 3000 AD </h2>
+    </div>
+    <br>
+        <div class="text-center">
+            <h5> The following years are LEAP </h5>
+        </div>
+        <br>
+        <div class="row">
+<?php
+        for($counter=1;$counter <= 3000; $counter++){
+            if($counter%4==0 && (($counter%100!==0) || $counter%400==0))   {
+            ?>
+            <div class="card col-sm-2">
+            <div class="card-header"> <?= $counter; ?> </div>
+            </div>
+        <?php
+            } 
+            ?>
+          
+        <?php
+        }
+        ?>
+        </div>
+
+        </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
